642109f5c213df561c4bd336b608a2daf1791786
khoivd <vudangkhoi1994@gmail.com>
2019-10-23 12:54:27 +0700

    remove console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js is up')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#msg-1')
